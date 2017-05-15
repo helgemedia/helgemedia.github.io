54f0c68162ae4ee0d4f7f2c9302025d6d8832a95
Julian Treutler <julian@julians-mbp.speedport_w_724v_09011603_05_010>
2017-05-15 10:10:21 +0200

    Team Page FadeIns & Contact Data

--- JS/team.js
+++ JS/team.js
@@ -3,14 +3,14 @@
  */
 
 $(document).ready(function () {
+
     $("#img1").hover(function () {
         $("#bio2").fadeIn(200);
-    }, function () {
-        $("#bio2").fadeOut(200);
     });
     $("#img2").hover(function () {
         $("#bio1").fadeIn(200);
-    }, function () {
-        $("#bio1").fadeOut(200);
+    });
+    $("body,#teamheader").not("#img1,#img2,#bio1,#bio2").hover(function () {
+        $("#bio2,#bio1").fadeOut(200);
     });
 });
